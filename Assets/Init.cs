ae68d3391fc1b5bbfa5978f0093d0b3edfaee0e5
Steve Detwiler <steve@n3twork.com>
2015-09-20 13:47:34 -0700

    First

---
+++ Assets/Init.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Init : MonoBehaviour {
+
+	public int NumSpiders = 1;
+	// Use this for initialization
+	void Start () {
+		for(int i=0; i<NumSpiders; ++i)
+		{
+			GameObject instance = Instantiate(Resources.Load("spider", typeof(GameObject))) as GameObject;
+			instance.transform.parent = gameObject.transform;
+			float s = 2.0f;
+			instance.transform.position = new Vector3(Random.Range(-s, s), 0.0f, Random.Range(-s, s));
+			float scale = 0.25f;
+			instance.transform.localScale = new Vector3(scale,scale,scale);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Walk.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk : MonoBehaviour {
+	private Vector3 target;
+	public float speed=0.1f;
+//	private SphereCollider objCollider;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Walk Start");
+		Animation anim = gameObject.GetComponent<Animation>();
+		foreach (AnimationState state in anim) {
+			if(state.name.Equals("walk"))
+			{
+//				state.speed = 0.8f;
+			}
+			Debug.Log (state.name);
+		}
+		anim.Play("walk");
+
+		PickTarget();
+	}
+
+	void PickTarget() {
+		float s = 10.0f;
+		Debug.Log ("PickTarget");
+		target = new Vector3(Random.Range(-s, s), 0.0f, Random.Range(-s, s));
+		Debug.Log(target);
+	}
+
+	void Update() {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+		float rotSpeed = 6.0f;
+		var targetRotation = Quaternion.LookRotation(target - transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotSpeed * Time.deltaTime);
+
+		if( transform.position == target)
+		{
+			PickTarget();
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log ("Collision");
+		Animation anim = gameObject.GetComponent<Animation>();
+		string attack = "attack" + Random.Range (1,2).ToString();
+		anim.Play (attack);
+		anim.PlayQueued("walk");
+		PickTarget();
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log ("Trigger");
+	}
+}
