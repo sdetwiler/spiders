d08247468bcfa9b17d4c3fb8db75dac07fae76ea
Steve Detwiler <steved@piratestudios.com>
2015-09-20 21:13:07 -0700

    Hacky but effective. controller support to move camera and rotate projection plane. Field of view adjustments and zoom, too.

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -5,17 +5,24 @@ public class InputController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
-	
+
+	Vector3 at = new Vector3(0,0,0);
+	// https://www.reddit.com/r/Unity3D/comments/1syswe/ps4_controller_map_for_unity/
+
 	// Update is called once per frame
 	void Update ()
 	{
-
+		Camera cam = gameObject.GetComponent<Camera> ();
+		GameObject plane = GameObject.Find ("House");
 
 		if (Input.GetButton ("PS4_PS")) {
 			Application.Quit();
 		}
+
+		// Depth of field.
+		float dpadX = Input.GetAxis ("PS4_DPadX");
+		cam.fieldOfView += dpadX;
 
 		// Translate camera.
 		float leftX = Input.GetAxis ("PS4_LeftStickX");
@@ -24,6 +31,11 @@ public class InputController : MonoBehaviour {
 		// Zoom camera.
 		float l2 = Input.GetAxis ("PS4_L2");
 		float r2 = Input.GetAxis ("PS4_R2");
+
+		// Rot camera.
+		float rightX = Input.GetAxis ("PS4_RightStickX");
+		float rightY = Input.GetAxis ("PS4_RightStickY");
+		Debug.LogFormat ("{0} {1}", rightX, rightY);
 
 		float dz = 0;
 		// L2 pressed
@@ -35,13 +47,34 @@ public class InputController : MonoBehaviour {
 			dz -= r2;
 		}
 
-//		float axis = Input.GetAxis (n);
-//		Debug.LogFormat ("{0}, {1}", l2, r2);
 		Vector3 newPos = gameObject.transform.position;
 		newPos.x += leftX;
 		newPos.y +=dz;
-		newPos.z += leftY;
-	
+		newPos.z -= leftY;
 		gameObject.transform.position = newPos;
+
+
+		Quaternion newPlaneRot = plane.transform.rotation;
+		Vector3 angles = plane.transform.rotation.eulerAngles;
+		angles.x += rightY;
+		angles.z += rightX;
+		newPlaneRot.eulerAngles = angles;
+		plane.transform.rotation = newPlaneRot;
+
+
+//		at.x += rightX;
+//		at.z += rightY;
+//		gameObject.transform.LookAt (at);
+//
+//		if (rightX!=0f || rightY!=0f) {
+//			Vector3 newRot = gameObject.transform.rotation.eulerAngles;
+//			newRot.z += rightX;
+//			newRot.y += rightY;
+//			
+//			Quaternion r = gameObject.transform.rotation;
+//			r.eulerAngles = newRot;
+//			
+//			gameObject.transform.rotation = r;
+//		}
 	}
 }
---
+++ Assets/RenderToggle.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderToggle : MonoBehaviour {
+
+	public bool EnableRendering = true;
+
+	private bool pressed = false;
+
+	// Use this for initialization
+	void Start () {
+		SetRenderingMode (EnableRendering);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("PS4_Triangle")) {
+			if (!pressed) {
+				pressed = true;
+				ToggleRendering ();
+			}
+		} else {
+			pressed = false;
+		}
+
+
+
+	
+	}
+
+	void SetRenderingMode(bool mode) {
+		MeshRenderer[] renderers = gameObject.GetComponentsInChildren<MeshRenderer>();
+
+		foreach (MeshRenderer r in renderers) {
+			r.enabled = mode;
+		}
+
+	}
+
+	void ToggleRendering() {
+		EnableRendering = !EnableRendering;
+		SetRenderingMode (EnableRendering);
+	}
+}
